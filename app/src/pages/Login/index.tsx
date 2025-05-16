e99803f949d03a678850eac2a170d29e4ac7300c
cemgthedev <cemg.the.dev@gmail.com>
2025-05-16 18:03:18 -0300

    feat: implementação da estrutura da página de login e validação dos campos
    a página de login foi implementada utilizando react hook form e zod para tipagem dos campos

--- app/src/pages/Login/index.tsx
+++ app/src/pages/Login/index.tsx
@@ -1,9 +1,19 @@
+import { Card, CardBody, CardHeader } from '@heroui/card';
+
+import { LoginForm } from './components/form';
+
 export default function LoginPage() {
   return (
-    <section className="flex flex-col items-center gap-4">
-      <div className="text-center justify-center">
-        <h1>Login</h1>
-      </div>
+    <section className="flex flex-col justify-center items-center gap-4 h-screen">
+      <Card className="w-1/3 px-2 py-4 max-lg:w-3/5 max-md:w-4/5">
+        <CardHeader className="flex flex-col items-center">
+          <h1 className="text-2xl font-semibold">Login</h1>
+          <p className="text-lg">Seja bem-vindo</p>
+        </CardHeader>
+        <CardBody>
+          <LoginForm />
+        </CardBody>
+      </Card>
     </section>
   );
 }
